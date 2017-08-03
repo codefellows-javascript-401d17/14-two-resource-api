95aa2cf3fb9fa5b3ddc4023616060fa0bd2028d0
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-03 15:57:38 -0700

    post route for a beer

--- route/beer-route.js
+++ route/beer-route.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser');
+const debug = require('brewery:beer router');
+const Brewery = require('../model/brewery.');
+
+const beerRouter = module.exports = new Router();
+
+beerRouter.post('/api/brewery/:breweryID/beer', jsonParser, function(req, res, next){
+  debug('POST: /api/brewery/:breweryID/beer');
+
+  Brewery.findByIdAndAddBeer(req.params.breweryID, req.body)
+  .then( beer => res.json(beer))
+  .catch(next);
+});
