8a96c41d46b843bea513b8151e2756bb3509cf85
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-03 14:30:16 -0700

    added dependancies to server.js, added connection to mongodb dbms

---
+++ lab-eddie/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const debug = require('debug');
+const Promise = require('bluebird');
+
+const app = express();
+const MONGODB_URI = 'mongodb://localhost/bands';
+const error = require('./middleware/errors.js');
+const bandRouter = require('./band-route.js');
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useMongoClient: true});
+
+app.use(cors());
+app.use(debug('dev'));
+app.use(bandRouter);
+app.use(error);
+
+app.listen(PORT, () => {
+  debug('Server active on port: ', PORT);
+})
