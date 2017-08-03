5d2b0bae28c534fe7ca0bc1cf4a1c58ad0c162e5
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-03 15:40:26 -0700

    finished server file

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 'use strict';
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
 const debug = require('debug')('brewery:server');
+const breweryRouter = require('./route/brewery-route.js');
+const beerRouter = require('./route/beer-route.js');
+const errors = require('./lib/error-middleware.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODG_URI = 'mongods://localhost/brewery';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODG_URI);
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(breweryRouter);
+app.use(beerRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`server on ${PORT}`);
