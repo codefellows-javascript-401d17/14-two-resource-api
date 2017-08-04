dd737aacd42c338ae2bcf6e306cfd2f3ac6effe0
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-03 22:48:27 -0700

    Wrote test for PUT route with valid body

--- lab-eddie/test/band-test.js
+++ lab-eddie/test/band-test.js
@@ -25,5 +25,30 @@ const modelAlbum = {
 }
 
 describe('Band Routes', function(){
-  
+  describe('POST /api/band', function(){
+    describe('With a valid req.body', () => {
+      after(done => {
+        if(this.tempBand) {
+          Album.remove({})
+          .then(() => done())
+          .catch(err => done(err));
+          return;
+        }
+        done();
+      })
+      it('Should return a body', (done) => {
+        console.log(`${url}/api/band`)
+        request.post(`${url}/api/band`)
+        .send(modelBand)
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('Led Zeppelin');
+          expect(res.body.homeTown).to.equal('London');
+          this.tempBand = res.body;
+          done()
+        })
+      })
+    });
+  });
 });
