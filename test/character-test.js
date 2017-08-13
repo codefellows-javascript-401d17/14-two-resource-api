41c45d92b66a10abf2c38b5795cb303b95d4fe82
Allan Liebold <allan.liebold@gmail.com>
2017-08-13 12:55:25 -0700

    adds put and delete tests

--- test/character-test.js
+++ test/character-test.js
@@ -13,11 +13,11 @@ const url = `http://localhost:${PORT}`;
 
 const exampleCharacter = {
   name: 'Paul'
-}
+};
 
 const updateCharacter = {
-  name: "Muad\'dib"
-}
+  name: 'Muad\'dib'
+};
 
 const exampleHouse = {
   name: 'Atreides',
@@ -25,7 +25,7 @@ const exampleHouse = {
   region: 'Delta Pavonis',
   words: 'Fear is the Mind-Killer',
   timestamp: new Date
-}
+};
 
 describe('Character Routes', function() {
   describe('POST: /api/house/:houseID/character', function() {
@@ -51,10 +51,46 @@ describe('Character Routes', function() {
       it('should return a character', done => {
         request.post(`${url}/api/house/${this.tempHouse._id}/character`)
         .send(exampleCharacter)
-        .end((err, res) => {
+        .end((err, response) => {
           if(err) return done(err);
-          expect(res.body.name).to.equal('Paul');
-          expect(res.body.houseID).to.equal(this.tempHouse._id.toString());
+          expect(response.body.name).to.equal('Paul');
+          expect(response.body.houseID).to.equal(this.tempHouse._id.toString());
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET: /api/house/:houseID/character/:id', function() {
+    describe('with a valid body', function() {
+      before(done => {
+        new House(exampleHouse).save()
+        .then(house => {
+          this.tempHouse = house;
+          return House.findByIdAndAddCharacter(house._id, exampleCharacter);
+        })
+        .then(character => {
+          this.tempCharacter = character;
+          done();
+        })
+        .catch(done);
+      });
+
+      after(done => {
+        Promise.all([
+          House.remove({}),
+          Character.remove({})
+        ])
+        .then(() => done())
+        .catch(done);
+      });
+
+      it('should return a character', done => {
+        request.get(`${url}/api/house/${this.tempHouse._id}/character/${this.tempCharacter._id}`)
+        .end((err, response) => {
+          if(err) return done(err);
+          expect(response.status).to.equal(200);
+          expect(response.body.name).to.equal('Paul');
           done();
         });
       });
@@ -67,23 +103,59 @@ describe('Character Routes', function() {
         new House(exampleHouse).save()
         .then(house => {
           this.tempHouse = house;
+          return House.findByIdAndAddCharacter(house._id, exampleCharacter);
+        })
+        .then(character =>  {
+          this.tempCharacter = character;
           done();
         })
         .catch(done);
       });
 
       after(done => {
-        if(this.tempHouse) {
-          House.remove({})
+        Promise.all([
+          House.remove({}),
+          Character.remove({})
+        ])
           .then(() => done())
           .catch(done);
-        }
-        done();
       });
 
       it('should return a character', done => {
+        request.put(`${url}/api/house/${this.tempHouse._id}/character/${this.tempCharacter._id}`)
+        .send(updateCharacter)
+        .end((err, response) => {
+          if(err) return done(err);
+          expect(response.status).to.equal(200);
+          expect(response.body.name).to.equal('Muad\'dib');
+          done();
+        });
+      });
+    });
+  });
 
-      })
-    })
-  })
+  describe('DELETE: /api/house/:houseID/character/:id', function(){
+    describe('with valid id', function() {
+      before(done => {
+        new House(exampleHouse).save()
+        .then(house => {
+          this.tempHouse = house;
+          return House.findByIdAndAddCharacter(house._id, exampleCharacter);
+        })
+        .then(character => {
+          this.tempCharacter = character;
+          done();
+        })
+        .catch(done);
+      });
+
+      it('should return 204 status', done => {
+        request.delete(`${url}/api/house/${this.tempHouse._id}/character/${this.tempCharacter._id}`)
+        .end((err, response) => {
+          expect(response.status).to.equal(204);
+          done();
+        });
+      });
+    });
+  });
 });
